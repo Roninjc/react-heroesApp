7368de1c8a792e57d0270f62de73972991e073c2
Jesus <jesus.castanocandela@gmail.com>
2021-08-26 21:16:12 +0200

    first commit

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,78 @@
+import React, { useContext } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
+
+export const Navbar = () => {
+
+    const { user:{ name }, dispatch } = useContext( AuthContext );
+    const history = useHistory();
+
+    const handleLogout = () => {
+
+        history.replace('/login');
+
+        dispatch({
+            type: types.logout
+        });
+    };
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link 
+                    className="navbar-brand" 
+                    to="/"
+                >
+                    Asociaciones
+                </Link>
+
+                <div className="collapse navbar-collapse">
+                    <div className="navbar-nav">
+
+                        <NavLink 
+                            activeClassName="active"
+                            className="nav-item nav-link" 
+                            exact
+                            to="/marvel"
+                        >
+                            Marvel
+                        </NavLink>
+
+                        <NavLink 
+                            activeClassName="active"
+                            className="nav-item nav-link" 
+                            exact
+                            to="/dc"
+                        >
+                            DC
+                        </NavLink>
+
+                        <NavLink 
+                            activeClassName="active"
+                            className="nav-item nav-link" 
+                            exact
+                            to="/search"
+                        >
+                            Search
+                        </NavLink>
+                    </div>
+                </div>
+
+                <ul className="navbar-nav">
+
+                    <span className="nav-item nav-link text-info">
+                        { name }
+                    </span>
+
+                    <button 
+                        className="nav-item nav-link btn"
+                        onClick={ handleLogout }
+                    >
+                        Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
+}
